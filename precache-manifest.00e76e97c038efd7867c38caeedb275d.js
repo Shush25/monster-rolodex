d4919588cfd215365d98495ec3caf5f6aa914806
Shush25 <shushant.kumar.singh.5@gmail.com>
2020-09-28 13:56:28 +0530

    Updates

---
+++ precache-manifest.00e76e97c038efd7867c38caeedb275d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e43f779e43a8b278a3e57883f2dd113",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c63f4eedd048cce0e441",
+    "url": "/monster-rolodex/static/css/main.f4ec71dc.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63f4eedd048cce0e441",
+    "url": "/monster-rolodex/static/js/main.5d29af24.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
